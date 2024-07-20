240b4914edd7ba443d3b21a2220931c816ce7e36
sneha090j <sneha090j@gmail.com>
2024-07-20 06:36:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
+import './App.css'
+
+function App() {
+ return (
+   <>
+   <div>E-Commerce </div>
+   <button>normal button</button>
+   <Button>BOOT</Button>
+
+   </>
+  )
+}
+
+export default App
